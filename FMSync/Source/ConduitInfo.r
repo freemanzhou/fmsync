8553fee6715d5fca016587851d3b5e39ac8e5e16
rtsuk <rtsuk>
2003-01-16 17:05:02 +0000

    Fix some project naming issues and change the location for the Palm Conduit SDK.

--- FMSync/Source/ConduitInfo.r
+++ FMSync/Source/ConduitInfo.r
@@ -9,7 +9,7 @@ resource 'CInf' (0)
 	nativeConduit,
 	0x0300,						// the version number for this conduit
 	'JFi5',						// Conduit "Creator" ID
-	"FMSync for JFile 5",		// user visible conduit name
+	"FMSync for JFile X",		// user visible conduit name
 	"FMSyncX",				// settings directory
 	defaultPriority,			// The conduit's priority
 	wantsUserInterface,			//  Does the conduit have any UI?
--- FMSync/Source/FMCVersion.r
+++ FMSync/Source/FMCVersion.r
@@ -2,86 +2,22 @@
 #include "MacTypes.r"
 #include "VersionDefinition.h"
 
-#if defined(BETA_RELEASE)
 resource 'vers' (1) {
 	0x2,
-	0x0,
-	beta,
-	0x4,
-	0,
-	"2.0b4",
-	"2.0b4, © 1998-2001, FMSync Software"
-};
-
-resource 'vers' (2) {
-	0x2,
-	0x0,
-	beta,
-	0x4,
-	0,
-	"2.0b4",
-	"FMSync for JFile 5 2.0b4"
-};
-
-#elif defined (ALPHA_RELEASE)
-resource 'vers' (1) {
-	0x2,
-	0x0,
-	alpha,
-	2,
-	0,
-	"2.0a3",
-	"2.0a3, © 1998-2001, FMSync Software"
-};
-
-resource 'vers' (2) {
-	0x2,
-	0x0,
-	alpha,
-	3,
-	0,
-	"2.0a3",
-	"FMSync for JFile 2.0a3"
-};
-#elif defined (DEMO_RELEASE)
-resource 'vers' (1) {
-	0x1,
-	0x15,
-	final,
-	0,
-	0,
-	"1.1.5 Demo",
-	"1.1.5 Demo, © 1998-2000, FMSync Software"
-};
-
-resource 'vers' (2) {
-	0x1,
-	0x15,
-	final,
-	0,
-	0,
-	"1.1.5 Demo",
-	"FMSync for JFile 5 2.0 Demo"
-};
-#else
-resource 'vers' (1) {
-	0x2,
-	0x0,
+	0x10,
 	development,
-	4,
+	0x1,
 	0,
-	"2.0d4",
-	"2.0d4, © 1998-2001, FMSync Software"
+	"2.1d1",
+	"2.1d1, © 1998-2002, FMSync Software"
 };
 
 resource 'vers' (2) {
 	0x2,
-	0x0,
+	0x10,
 	development,
-	4,
+	0x1,
 	0,
-	"2.0d4",
-	"FMSync for JFile 2.0d4"
+	"2.1d1",
+	"FMSync for JFile X 2.1d1"
 };
-#endif
-
