0400d3d4172c115ebbb3119ef17b838a2af94470
rtsuk <rtsuk>
2003-01-17 16:25:47 +0000

    Updated version number for release.

--- FMSync/Source/FMCVersion.r
+++ FMSync/Source/FMCVersion.r
@@ -6,18 +6,18 @@ resource 'vers' (1) {
 	0x2,
 	0x10,
 	development,
-	0x1,
+	0x2,
 	0,
-	"2.1d1",
-	"2.1d1, © 1998-2002, FMSync Software"
+	"2.1d2",
+	"2.1d2, © 1998-2003, FMSync Software"
 };
 
 resource 'vers' (2) {
 	0x2,
 	0x10,
 	development,
-	0x1,
+	0x2,
 	0,
-	"2.1d1",
-	"FMSync for JFile X 2.1d1"
+	"2.1d2",
+	"FMSync for JFile X 2.1d2"
 };
