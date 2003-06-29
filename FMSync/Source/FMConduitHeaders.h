934c258e4ccd315461e4b1f3151800dbe7273a83
rtsuk <rtsuk>
2003-06-29 19:07:23 +0000

    *** empty log message ***

--- FMSync/Source/FMConduitHeaders.h
+++ FMSync/Source/FMConduitHeaders.h
@@ -1,15 +1,1 @@
-// ===========================================================================
-//	PP_DebugHeaders.h				©1996 Metrowerks Inc. All rights reserved.
-// ===========================================================================
-
-	// Use PowerPlant-specific Precompiled header
-	
-#if __POWERPC__
-	#include "FMConduitHeadersPPC"
-	
-#elif __CFM68K__
-	#include "FMConduitHeadersCFM68K"
-	
-#else
-	#include "FMConduitHeaders68K"
-#endif
+#include "FMConduitHeadersCarbon"
